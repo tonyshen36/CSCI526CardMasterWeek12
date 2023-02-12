226f538a358b9b36e89c380b70889c94c91040a4
Tsung-Lin Shen <44684888+tonyshen36@users.noreply.github.com>
2023-02-12 14:22:29 -0800

    Merge Several Push

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Transform player;
+    private Vector3 CamaeraPos;
     [SerializeField] Vector3 offset;
+    private float CamaeraSpeed = 1;
 
     private void Start()
     {
         player = PlayerController.instance.transform;
+        CamaeraPos = player.position + offset;
+        this.transform.position = CamaeraPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.position + offset;
+        if (Input.GetKey(KeyCode.W))
+        {
+            CamaeraPos.y += CamaeraSpeed / 100;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            CamaeraPos.y -= CamaeraSpeed / 100;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            CamaeraPos.x += CamaeraSpeed / 100;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            CamaeraPos.x -= CamaeraSpeed / 100;
+        }
+        this.transform.position = CamaeraPos;
     }
 }
---
+++ Assets/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+    public bool s2 = false;
+
+    void Start()
+    {
+        cam1.SetActive(true);
+        cam2.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            s2 = !s2;
+            cam2.SetActive(s2);
+            Debug.Log("s2 " + s2);
+        }
+    }
+}
