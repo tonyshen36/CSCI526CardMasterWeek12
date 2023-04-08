df78f11d5881a93a48f8216de66dfc4162bf2b6e
Bai Tianyu <tbai@usc.edu>
2023-04-08 00:36:32 -0700

    Update level 2.5
    Existing bug:
    platform either stop by wall or can't allow collision with player
    
    Fixed:
    Lower difficulty of level 1

--- Assets/Scripts/Trap/PlatformMovement.cs
+++ Assets/Scripts/Trap/PlatformMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,28 @@ public class PlatformMovement : MonoBehaviour
     public float speed = 5.0f; // the speed at which the object moves
     private int direction = 1; // the direction the object is currently moving (-1 = left, 1 = right)
     public bool isCollide = false;
+    public bool horizontal = false;
 
     void Update()
     {
-        // move the object left or right based on the current direction and speed
-        transform.Translate(new Vector3(direction * speed * Time.deltaTime, 0, 0));
-        if(isCollide)
+        if (horizontal)
         {
-            PlayerController.instance.transform.Translate(new Vector3(direction * speed * Time.deltaTime, 0, 0));
+            transform.Translate(new Vector3(0, direction * speed * Time.deltaTime, 0));
+            if (isCollide)
+            {
+                PlayerController.instance.transform.Translate(new Vector3(0, direction * speed * Time.deltaTime, 0));
+            }
         }
+        else
+        {
+            // move the object left or right based on the current direction and speed
+            transform.Translate(new Vector3(direction * speed * Time.deltaTime, 0, 0));
+            if (isCollide)
+            {
+                PlayerController.instance.transform.Translate(new Vector3(direction * speed * Time.deltaTime, 0, 0));
+            }
+        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -36,12 +50,15 @@ public class PlatformMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // check if the object has collided with the left or right wall
-        if (collision.gameObject.CompareTag("Wall"))
+        if (collision.gameObject.CompareTag("pCollider"))
         {
-            // reverse the direction of movement
             direction *= -1;
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            Debug.Log("pCollider");
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isCollide = true;
         }
     }
 
