8e4f9c6453233b91ea9665dbda55d1eff87a564a
1eom3i <meix@usc.edu>
2023-04-09 18:05:08 -0700

    Tutorial

--- Assets/Scripts/Door/DoorController2.cs
+++ Assets/Scripts/Door/DoorController2.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DoorController2 : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject door;
+
+    public bool isClose = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class DoorController2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isClose)
+        {
+            if (door.transform.position.x<160)
+            {
+                door.SetActive(false);
+            }
+            else
+            {
+                isClose = true;
+                door.SetActive(true);
+            }
+        }
         
     }
 }
